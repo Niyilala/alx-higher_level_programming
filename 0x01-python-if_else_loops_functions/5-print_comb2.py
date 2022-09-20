a7a1c9d6004646cd13dd9fc7c1dcab501b579232
Niyilala <adeniyi.oluwajana@gmail.com>
2022-09-20 15:11:39 -0700

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-last_digit = number % -10 if number < 0 else number % 10
-if last_digit > 5:
-    result = "greater than 5"
-elif last_digit == 0:
-    result = "0"
-else:
-    result = "less than 6 and not 0"
-print(f"Last digit of {number:d}", f"is {last_digit:d} and is", result)
+for i in range(100):
+    print("{:d}".format(i).rjust(2, "0"),
+          end="\n" if i == 99 else ", ")
